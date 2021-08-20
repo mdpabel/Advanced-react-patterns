c475cecaa1e2e9be8cafd00325e25b791c43ede7
mdpabel <mdpabel385@gmail.com>
2021-08-21 01:14:01 +0600

    Advanced react patterns

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import ReactDOM from "react-dom";
+import App from "./07. State reducer/App";
+
+import "./App.css";
+import "./index.css";
+
+ReactDOM.render(
+  <>
+    <App />
+  </>,
+  document.getElementById("root")
+);
